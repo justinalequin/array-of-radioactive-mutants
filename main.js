77915798c4a3de30d6e4d1fcfd0f558ec5713e6c
justinalequin <justinalequin@gmail.com>
2021-06-29 15:09:58 -0400

    Update! All tests passed.

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd(arr){
+arr.push("Colin")
+return arr
+}
+addMeToEnd(["One", "Two", "Three"])
 
+
+function addMeToStart(arr1){
+  arr1.unshift("Colin")
+return arr1
+} addMeToStart(["One", "Two", "Three"])
+
+function changeLast(arr2, givenNumber){
+  //let givenNumber = 5;
+  arrLen = Number(arr2.length - 1)
+  arr2.splice((arrLen), 1, givenNumber)
+  return arr2
+} changeLast([1, 2, 3])
+
+function changeAllValuesTo(arr3, newValue){
+  for (i = 0; i < arr3.length; i++){
+    arr3[i] = newValue
+  }return arr3
+}
+
+function oddOrEven(arr4){
+  for (i = 0; i < arr4.length; i++){
+    if (arr4[i] % 2 === 0){
+      arr4[i] = "even"
+    } else if (arr4[i] % 2 !== 0){
+      arr4[i] = "odd"
+    }
+  }
+  return arr4
+}
+
+function changeNextThreeToValue( start1, arr5, value1){
+
+  for (i = start1; i < start1 + 3; i++){
+    arr5[i] = value1
+  }
+
+
+} 
 
 
 
